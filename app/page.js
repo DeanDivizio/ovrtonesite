25649b1bf18b6b10cd302b05f4edd184b363271b
Dean Divizio <dean@deandivizio.com>
2024-04-10 14:03:04 -0400

    changed resource urls to api subdomain

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
    <div className={styles.homeContainer}>
       <div className={styles.homeContent}>
-        <img src="https://ovrtonemedia.com/wp-content/uploads/2024/04/OVRTONE-Logo_White_Trans_NoText_MedRes.png" alt="OVRTONE Media Group" />
+        <img src="https://api.ovrtonemedia.com/wp-content/uploads/2024/04/OVRTONE-Logo_White_Trans_NoText_MedRes.png" alt="OVRTONE Media Group" />
         <h1>oVRTONE</h1>
         <h2>Media Group</h2>
         <Link href="mailto:contact@ovrtonemedia.com">contact@ovrtonemedia.com</Link>
