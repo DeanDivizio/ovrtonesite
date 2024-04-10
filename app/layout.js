499e81ff95f0e6ed217c5f777a77edc94be9172b
Dean Divizio <dean@deandivizio.com>
2024-04-10 13:55:44 -0400

    implemented initial animation

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import IntroAnimation from "../components/IntroAnimation";
 
 export const metadata = {
   title: "OVRTONE Media Group",
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className='layoutContainer'>
+      <IntroAnimation />
       <video autoPlay loop="loop" muted id="bgVid">
         <source src="https://ovrtonemedia.com/wp-content/uploads/2023/04/OVRTONE-BG-Vid.mov" type="video/quicktime"/>
         </video>
---
+++ components/IntroAnimation.js
@@ -0,0 +1,40 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import styles from './introAnimation.module.css';
+import Link from 'next/link';
+
+const IntroAnimation = () => {
+    const [visible, setVisible] = useState(true);
+    const [shouldRender, setShouldRender] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setVisible(false);
+            const removeTimer = setTimeout(() => {
+                setShouldRender(false);
+            }, 1000); // Delay for fade-out animation
+            return () => clearTimeout(removeTimer);
+        }, 2000); // Total duration of the animation
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (!shouldRender) {
+        return null;
+    }
+
+    const containerClass = visible ? styles.homeContainer : `${styles.homeContainer} ${styles.fadeOut}`;
+
+    return (
+        <div className={containerClass}>
+            <div className={styles.homeContent}>
+                <img src="https://ovrtonemedia.com/wp-content/uploads/2024/04/OVRTONE-Logo_Black_Trans_NoText_MedRes.png" alt="OVRTONE Media Group" />
+                <h1>oVRTONE</h1>
+                <h2>Media Group</h2>
+                <Link href="mailto:contact@ovrtonemedia.com">contact@ovrtonemedia.com</Link>
+            </div>
+        </div>
+    );
+};
+
+export default IntroAnimation;
