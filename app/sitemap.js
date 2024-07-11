60d7f484a8b61c5ca3b1d4da648c9212bb5aa116
Dean Divizio <dean@deandivizio.com>
2024-07-11 13:49:46 -0400

    added sitemap and robots

---
+++ app/sitemap.js
@@ -0,0 +1,70 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://www.ovrtonemedia.com',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+      },
+      {
+        url: 'https://acme.com/about',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://acme.com/web',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://acme.com/media',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://acme.com/branding',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://acme.com/contact',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://acme.com/pricing',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://acme.com/about',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://acme.com/privacy',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.1,
+      },
+      {
+        url: 'https://acme.com/terms',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.1,
+      },
+      {
+        url: 'https://acme.com/thankyou',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0,
+      },
+    ]
+  }
